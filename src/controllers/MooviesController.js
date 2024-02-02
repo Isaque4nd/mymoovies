d672be47ff75b0ded738ba0a83b90b173ae99f37
Isaque Ribeiro de Andrade <andradeisq1.7@gmail.com>
2024-02-02 20:15:46 -0300

    Update MooviesController.js

--- src/controllers/MooviesController.js
+++ src/controllers/MooviesController.js
@@ -47,16 +47,24 @@ const MooviesController = {
 
   async create(req, res) {
     const { title, description, category_id, realease_date } = req.body;
-
-    // É necessário realizar uma validação pelo id de categoria
-
+  
+    // Validação de categoria existente
+    const category = await db.query("SELECT * FROM category WHERE id = $1", [
+      category_id,
+    ]);
+  
+    if (category.rowCount === 0) {
+      res.status(404).json({ error: "Categoria não encontrada" });
+      return;
+    }
+  
     try {
       const newMoovie = await db.query(
         `INSERT INTO moovie (title, description, category_id, realease_date)
          VALUES ($1, $2, $3, $4) RETURNING *`,
         [title, description, category_id, realease_date]
       );
-
+  
       res.status(201).json(newMoovie.rows[0]);
     } catch (error) {
       res.status(500).json({ error: error.message });
@@ -81,6 +89,26 @@ const MooviesController = {
       res.status(500).json({ error: error.message });
     }
   },
+
+  async update(req, res) {
+    const { id } = req.params;
+    const { title, description, category_id, realease_date } = req.body;
+  
+    try {
+      const result = await db.query(
+        "UPDATE moovie SET title = $1, description = $2, category_id = $3, realease_date = $4 WHERE id = $5 RETURNING *",
+        [title, description, category_id, realease_date, id]
+      );
+  
+      if (result.rowCount > 0) {
+        res.status(200).json(result.rows[0]);
+      } else {
+        res.status(404).json({ error: "Filme não encontrado" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
 };
 
 module.exports = MooviesController;
